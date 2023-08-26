9fee3f23f27cbfe3592b6aaf988014d9d2f6f50c
Kelvin Samuel Klutse <88723961+Vankelv@users.noreply.github.com>
2023-08-26 13:11:48 +0000

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,15 +3,6 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const corsOptions = {
-<<<<<<< Updated upstream
-  origin: 'https://who-invent-what.vercel.app',
-=======
-  origin: 'http://localhost:5173/',
->>>>>>> Stashed changes
-  methods: ['GET', 'POST'],
-  credentials: true,
-};
 
 app.use(cors(corsOptions));
 app.use(express.json());

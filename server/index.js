d30235a8f890d5be1c5b4a26f1d97be6c72c6e7b
Kelvin Samuel Klutse <88723961+Vankelv@users.noreply.github.com>
2023-08-26 13:12:54 +0000

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,41 +1,36 @@
-const { MongoClient } = require("mongodb");
-const express = require('express');
-const cors = require('cors');
+const { MongoClient, ObjectId } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 const corsOptions = {
-<<<<<<< Updated upstream
-  origin: 'https://who-invent-what.vercel.app',
-=======
-  origin: 'http://localhost:5173/',
->>>>>>> Stashed changes
-  methods: ['GET', 'POST'],
+  origin: ["https://who-invent-what.vercel.app", "http://172.20.10.5:5173"],
+  methods: ['GET,HEAD,PUT,PATCH,POST,DELETE'],
   credentials: true,
 };
 
 app.use(cors(corsOptions));
+
 app.use(express.json());
 
 app.use(cors(corsOptions));
 app.use(express.json());
 
 const DB_User = encodeURIComponent("vankelvin603");
-const Db_pass = encodeURIComponent('0546Van')
+const Db_pass = encodeURIComponent("0546Van");
 const uri = `mongodb+srv://${DB_User}:${Db_pass}@who-invent-what.wh0vdyz.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri);
 
+let db; // Reference to the MongoDB database //
 
-let db; // Reference to the MongoDB database
-
-async function connect(){
+async function connect() {
   db = client.db("Whoinventwhat");
-  try{
+  try {
     await client.connect(uri);
-    console.log("Connected to MongoDb")
+    console.log("Connected to MongoDb");
   } catch (error) {
     console.error(error);
   }
-
 }
 connect();
 
@@ -43,7 +38,6 @@ app.get("/", (req, res) => {
   return res.json({ message: "From server side" });
 });
 
- 
 app.get("/users", async (req, res) => {
   try {
     const users = await db.collection("users").find().toArray();
@@ -65,10 +59,12 @@ app.get("/categories", async (req, res) => {
 app.get("/inventions", async (req, res) => {
   try {
     const inventions = await db.collection("inventions").find().toArray();
-    res.set('Access-Control-Allow-Origin', '*'); // Change 'response' to 'res'
+    res.set("Access-Control-Allow-Origin", "*"); // Change 'response' to 'res'
     return res.json(inventions);
   } catch (err) {
-    return res.status(500).json({ error: "Failed to fetch inventions from MongoDB" });
+    return res
+      .status(500)
+      .json({ error: "Failed to fetch inventions from MongoDB" });
   }
 });
 
@@ -76,23 +72,23 @@ app.get("/inventions", async (req, res) => {
 app.get("/inventionsByYear", async (req, res) => {
   const year = parseInt(req.query.year) || 2015;
   const limit = parseInt(req.query.limit) || 10; // Default limit is set to 10, change as needed
-  res.set('Access-Control-Allow-Origin', '*');
+  res.set("Access-Control-Allow-Origin", "*");
 
   try {
-    const inventions = await db.collection("inventions")
+    const inventions = await db
+      .collection("inventions")
       .find({ year: { $gte: year } })
-      .limit(limit) // Apply the limit to the database query
+      .limit(limit)
       .toArray();
 
     return res.json(inventions);
   } catch (err) {
     console.error("Error fetching inventions by year:", err);
-    return res.status(500).json({ error: "Failed to fetch inventions by year" });
+    return res
+      .status(500)
+      .json({ error: "Failed to fetch inventions by year" });
   }
 });
-
-
-
 
 app.post("/inventions", async (req, res) => {
   const { inventionName, inventor, year, category, country } = req.body;
@@ -116,6 +112,58 @@ app.post("/inventions", async (req, res) => {
     return res.status(500).json({ error: "Failed to submit invention data" });
   }
 });
+
+//Invention actions. Delete and Edit
+
+app.put("/inventions/:id", async (req, res) => {
+  const inventionId = req.params.id;
+  const { inventionName, inventor, year, category, country } = req.body;
+
+  try {
+    const result = await db.collection("inventions").updateOne(
+      { _id: ObjectId(inventionId) }, // Use 'ObjectId' correctly
+      {
+        $set: {
+          inventionName,
+          inventor,
+          year,
+          category,
+          country,
+        },
+      }
+    );
+
+    if (result.matchedCount === 1) {
+      return res.json({ message: "Invention updated successfully" });
+    } else {
+      return res.status(404).json({ error: "Invention not found" });
+    }
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ error: "Failed to update invention" });
+  }
+});
+
+
+app.delete("/inventions/:id", async (req, res) => {
+  const inventionId = req.params.id;
+
+  try {
+    const result = await db
+      .collection("inventions")
+      .deleteOne({ _id: ObjectId(inventionId) });
+
+    if (result.deletedCount === 1) {
+      return res.json({ message: "Invention deleted successfully" });
+    } else {
+      return res.status(404).json({ error: "Invention not found" });
+    }
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ error: "Failed to delete invention" });
+  }
+});
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {

0ab187899ee08167697265be4acdb16689dd22d9
Kelvin Samuel Klutse <88723961+Vankelv@users.noreply.github.com>
2023-10-09 20:32:01 +0000

    Update index.js

--- server/index.js
+++ server/index.js
@@ -22,15 +22,15 @@ const client = new MongoClient(uri);
 let db; 
 
 async function connect() {
+  db = client.db("Whoinventwhat");
   try {
-    await client.connect(); // Open the connection
-    db = client.db("Whoinventwhat");
-    console.log("Connected to MongoDB");
+    await client.connect(uri);
+    console.log("Connected to MongoDb");
   } catch (error) {
     console.error(error);
   }
 }
-
+connect();
 
 app.get("/", (req, res) => {
   return res.json({ message: "From server side" });

40bb6e6ef9203ee5fe905e431a1694a353e31c88
Kelvin Samuel Klutse <nextlinedesigns@gmail.com>
2023-11-07 10:08:50 +0000

    Revert "Update index.js"
    This reverts commit 1be439404f0488fceb45500785835d6d0d17e947.

--- server/index.js
+++ server/index.js
@@ -175,7 +175,7 @@ app.delete('/inventions/:_id', (req, res) => {
         res.status(500).json({ error: 'Could not delete the invention' });
       });
   } else {
-    res.status(500).json({ error: 'Not a valid inventions id' });
+    res.status(500).json({ error: 'Not a valid inventions _id' });
   }
 });
 
